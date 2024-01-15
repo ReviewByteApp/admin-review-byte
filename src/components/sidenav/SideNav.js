285579f80dab4bf30555e85db460024a4a51bcb3
antish24 <aantenehh@gmail.com>
2024-01-14 16:24:34 -0800

    first commit

---
+++ src/components/sidenav/SideNav.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import styles from './sidenav.module.css'
+
+const SideNav = () => {
+  return (
+    <div className={styles.cont}>
+        sidenav
+    </div>
+  )
+}
+
+export default SideNav
---
+++ src/pages/register/Register.js
@@ -0,0 +1,10 @@
+import React from 'react'
+import styles from './register.module.css'
+
+const Register = () => {
+  return (
+    <div className={styles.cont}></div>
+  )
+}
+
+export default Register
