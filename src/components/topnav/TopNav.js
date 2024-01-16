09d5d74f47861c7b439d4c10384c501b71445568
antish24 <aantenehh@gmail.com>
2024-01-15 20:52:33 -0800

    vid then pic

--- src/components/topnav/TopNav.js
+++ src/components/topnav/TopNav.js
@@ -1,11 +1,21 @@
 import React from 'react'
 import styles from './topnav.module.css'
+import {useLocation} from 'react-router-dom'
 
 const TopNav = () => {
+  const {pathname}=useLocation()
+
   return (
     <div className={styles.cont}>
       <div className={styles.leftbox}></div>
-      <div className={styles.rightbox}></div>
+      <div className={styles.rightbox}>
+        <span className={styles.navname}>{pathname.substring(1)}</span>
+        <div className={styles.minnav}>
+          <button>add</button>
+          <button>add2</button>
+          <button>add3</button>
+        </div>
+      </div>
     </div>
   )
 }
--- src/pages/setting/Setting.js
+++ src/pages/setting/Setting.js
@@ -1,58 +1,38 @@
 import React, { useState } from 'react'
 import styles from './setting.module.css'
-import { IoBusiness } from "react-icons/io5";
+import { IoBusiness, IoFileTrayFullSharp } from "react-icons/io5";
 import { MdOutlineExpandLess, MdOutlineExpandMore } from "react-icons/md";
 import CompanyBasicInfoForm from '../../components/forms/BasicInfo';
 import CompanyContactInfo from '../../components/forms/ContactInfo';
 import CompanyDescriptionInfo from '../../components/forms/DesInfo';
 import CompanyIntroInfo from '../../components/forms/Intro';
-
+import { FaMapMarkedAlt } from 'react-icons/fa';
+import { TbListDetails } from "react-icons/tb";
 const Setting = () => {
   
-  const [showMore,setShowMore]=useState(false)
-  const [showMore2,setShowMore2]=useState(false)
-  const [showMore3,setShowMore3]=useState(false)
-  const [showMore4,setShowMore4]=useState(false)
+  const [showMore,setShowMore]=useState('')
+
+  const tabs=[
+    {id:1,title:'Basic Info',icon:<IoBusiness/>,content:<CompanyBasicInfoForm/>},
+    {id:2,title:'Company Intro',icon:<IoFileTrayFullSharp />,content:<CompanyIntroInfo/>},
+    {id:3,title:'Company Description',icon:<TbListDetails/>,content:<CompanyDescriptionInfo/>},
+    {id:4,title:'Contact Info',icon:<FaMapMarkedAlt/>,content:<CompanyContactInfo/>},
+  ]
 
   return (
     <div className={styles.cont}>
       <div className={styles.box}>
-        <div className={styles.basicinfo} style={{height:showMore?'max-content':'50px'}}>
-          <div className={styles.topbox} onClick={()=>setShowMore(c=>!c)}>
-            <span className={styles.title}><IoBusiness/>Basic Info</span>
-            {showMore?<MdOutlineExpandLess className={styles.openmore}/>:<MdOutlineExpandMore className={styles.openmore}/>}
+        {tabs.map((l)=>(
+          <div className={styles.basicinfo} style={{height:showMore===l.id?'max-content':'50px',borderColor:l.id===4?'transparent':'gray'}}>
+          <div className={styles.topbox} onClick={showMore===l.id?()=>setShowMore(''):()=>setShowMore(l.id)}>
+            <span className={styles.title}>{l.icon}{l.title}</span>
+            {showMore===l.id?<MdOutlineExpandLess className={styles.openmore}/>:<MdOutlineExpandMore className={styles.openmore}/>}
           </div>
-          {showMore &&
-          <CompanyBasicInfoForm/>
+          {showMore===l.id &&
+          l.content
         }
         </div>
-        <div className={styles.basicinfo} style={{height:showMore2?'max-content':'50px'}}>
-          <div className={styles.topbox} onClick={()=>setShowMore2(c=>!c)}>
-            <span className={styles.title}><IoBusiness/>Company Intro</span>
-            {showMore2?<MdOutlineExpandLess className={styles.openmore}/>:<MdOutlineExpandMore className={styles.openmore}/>}
-          </div>
-          {showMore2 &&
-          <CompanyIntroInfo/>
-        }
-        </div>
-        <div className={styles.basicinfo} style={{height:showMore3?'max-content':'50px'}}>
-          <div className={styles.topbox} onClick={()=>setShowMore3(c=>!c)}>
-            <span className={styles.title}><IoBusiness/>Company description</span>
-            {showMore3?<MdOutlineExpandLess className={styles.openmore}/>:<MdOutlineExpandMore className={styles.openmore}/>}
-          </div>
-          {showMore3 &&
-          <CompanyDescriptionInfo/>
-        }
-        </div>
-        <div className={styles.contactinfo} style={{height:showMore4?'max-content':'50px'}}>
-          <div className={styles.topbox} onClick={()=>setShowMore4(c=>!c)}>
-            <span className={styles.title}><IoBusiness/>Contact Info</span>
-            {showMore4?<MdOutlineExpandLess className={styles.openmore}/>:<MdOutlineExpandMore className={styles.openmore}/>}
-          </div>
-          {showMore4 &&
-          <CompanyContactInfo/>
-          }
-        </div>
+        ))}
       </div>
     </div>
   )
